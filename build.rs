c05aeb5b31c77e52fd88a0e471e0b4d576761d47
Alexander Sagen <alexander@sagen.me>
2021-04-25 14:29:30 +0200

    Expose internal API

--- build.rs
+++ build.rs
@@ -14,10 +14,11 @@ fn main() {
 	let out_dir = PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR var not set"));
 	let build_dir = out_dir.join("build").join("libnfc");
 	let include_dir = out_dir.join("include");
+	let nfc_dir = vendor_dir.join("nfc");
+	let libnfc_dir = nfc_dir.join("libnfc");
 
 	// Build libnfc and link against it
 	fs::create_dir_all(&out_dir).unwrap();
-	let nfc_dir = vendor_dir.join("nfc");
 	cmake::Config::new(&nfc_dir)
 		.define("DLLTOOL", &env::var("DLLTOOL").unwrap_or(String::from("dlltool")))
 		.define("LIBUSB_INCLUDE_DIRS", &usb01_include_dir)
@@ -45,11 +46,38 @@ fn main() {
 
 	// Generate libnfc bindings
 	let bindings = Builder::default()
-		.header(include_dir.join("nfc").join("nfc.h").to_str().unwrap())
 		.clang_arg(format!("-I{}", include_dir.display()))
+		.clang_arg(format!("-I{}", libnfc_dir.display()))
+		.header(include_dir.join("nfc").join("nfc.h").to_str().unwrap())
+		.allowlist_function("iso14443a_.*")
 		.allowlist_function("nfc_.*")
 		.allowlist_type("nfc_.*")
 		.allowlist_var("NFC_.*")
+		.header(libnfc_dir.join("iso7816.h").to_str().unwrap())
+		.allowlist_var("ISO7816_.*")
+		.header(libnfc_dir.join("drivers").join("acr122_pcsc.h").to_str().unwrap())
+		.header(libnfc_dir.join("drivers").join("acr122_usb.h").to_str().unwrap())
+		.header(libnfc_dir.join("drivers").join("acr122s.h").to_str().unwrap())
+		.allowlist_function("acr122s?_.*")
+		.allowlist_type("acr122s?_.*")
+		.allowlist_var("ACR122S?_.*")
+		.header(libnfc_dir.join("drivers").join("arygon.h").to_str().unwrap())
+		.allowlist_function("arygon_.*")
+		.allowlist_type("arygon_.*")
+		.allowlist_var("ARYGON_.*")
+		.header(libnfc_dir.join("chips").join("pn53x.h").to_str().unwrap())
+		.header(libnfc_dir.join("drivers").join("pn53x_usb.h").to_str().unwrap())
+		.header(libnfc_dir.join("drivers").join("pn532_i2c.h").to_str().unwrap())
+		.header(libnfc_dir.join("drivers").join("pn532_spi.h").to_str().unwrap())
+		.header(libnfc_dir.join("drivers").join("pn532_uart.h").to_str().unwrap())
+		.header(libnfc_dir.join("drivers").join("pn71xx.h").to_str().unwrap())
+		.allowlist_function("pn(53[x23]|71xx)_.*")
+		.allowlist_type("pn(53[x23]|71xx)_.*")
+		.allowlist_var("PN(53[Xx23]|71XX)_.*")
+		.header(libnfc_dir.join("drivers").join("pcsc.h").to_str().unwrap())
+		.allowlist_function("pcsc_.*")
+		.allowlist_type("pcsc_.*")
+		.allowlist_var("PCSC_.*")
 		.generate()
 		.expect("Unable to generate nfc bindings");
 
