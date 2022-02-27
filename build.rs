64c3521712b752a0e9d70bbb6ba8643eb461d852
Naoki Ikeguchi <me@s6n.jp>
2022-02-27 18:32:01 +0900

    feat: Include chipset or driver specific symbols by drivers feature

--- build.rs
+++ build.rs
@@ -117,7 +117,6 @@ fn main() {
 	let vendor_dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR var not set")).join("vendor");
 	let out_dir = PathBuf::from(env::var("OUT_DIR").expect("OUT_DIR var not set"));
 	let nfc_dir = vendor_dir.join("nfc");
-	let libnfc_dir = nfc_dir.join("libnfc");
 	let libnfc_pkg = if cfg!(feature = "vendored") {
 		make_source(&nfc_dir, &out_dir)
 	} else {
@@ -140,42 +139,48 @@ fn main() {
 	}
 
 	// Generate libnfc bindings
-	let bindings = Builder::default()
-		.clang_arg(format!("-I{}", libnfc_dir.display()))
+	bindings = bindings
 		.allowlist_function("iso14443[ab]_.*")
 		.allowlist_function("str_nfc_.*")
 		.allowlist_function("nfc_.*")
 		.allowlist_type("nfc_.*")
-		.allowlist_var("NFC_.*")
-		.header(libnfc_dir.join("iso7816.h").to_str().unwrap())
-		.allowlist_var("ISO7816_.*")
-		.header(libnfc_dir.join("drivers").join("acr122_pcsc.h").to_str().unwrap())
-		.header(libnfc_dir.join("drivers").join("acr122_usb.h").to_str().unwrap())
-		.header(libnfc_dir.join("drivers").join("acr122s.h").to_str().unwrap())
-		.allowlist_function("acr122s?_.*")
-		.allowlist_type("acr122s?_.*")
-		.allowlist_var("ACR122S?_.*")
-		.header(libnfc_dir.join("drivers").join("arygon.h").to_str().unwrap())
-		.allowlist_function("arygon_.*")
-		.allowlist_type("arygon_.*")
-		.allowlist_var("ARYGON_.*")
-		.header(libnfc_dir.join("chips").join("pn53x.h").to_str().unwrap())
-		.header(libnfc_dir.join("drivers").join("pn53x_usb.h").to_str().unwrap())
-		.header(libnfc_dir.join("drivers").join("pn532_i2c.h").to_str().unwrap())
-		.header(libnfc_dir.join("drivers").join("pn532_spi.h").to_str().unwrap())
-		.header(libnfc_dir.join("drivers").join("pn532_uart.h").to_str().unwrap())
-		.header(libnfc_dir.join("drivers").join("pn71xx.h").to_str().unwrap())
-		.allowlist_function("pn(53[x23]|71xx)_.*")
-		.allowlist_type("pn(53[x23]|71xx)_.*")
-		.allowlist_var("PN(53[Xx23]|71XX)_.*")
-		.header(libnfc_dir.join("drivers").join("pcsc.h").to_str().unwrap())
-		.allowlist_function("pcsc_.*")
-		.allowlist_type("pcsc_.*")
-		.allowlist_var("PCSC_.*")
-		.generate()
-		.expect("Unable to generate nfc bindings");
+		.allowlist_var("NFC_.*");
+
+	if cfg!(feature = "drivers") {
+		let libnfc_dir = nfc_dir.join("libnfc");
+
+		bindings = bindings
+			.clang_arg(format!("-I{}", libnfc_dir.display()))
+			.header(libnfc_dir.join("iso7816.h").to_str().unwrap())
+			.allowlist_var("ISO7816_.*")
+			.header(libnfc_dir.join("drivers").join("acr122_pcsc.h").to_str().unwrap())
+			.header(libnfc_dir.join("drivers").join("acr122_usb.h").to_str().unwrap())
+			.header(libnfc_dir.join("drivers").join("acr122s.h").to_str().unwrap())
+			.allowlist_function("acr122s?_.*")
+			.allowlist_type("acr122s?_.*")
+			.allowlist_var("ACR122S?_.*")
+			.header(libnfc_dir.join("drivers").join("arygon.h").to_str().unwrap())
+			.allowlist_function("arygon_.*")
+			.allowlist_type("arygon_.*")
+			.allowlist_var("ARYGON_.*")
+			.header(libnfc_dir.join("chips").join("pn53x.h").to_str().unwrap())
+			.header(libnfc_dir.join("drivers").join("pn53x_usb.h").to_str().unwrap())
+			.header(libnfc_dir.join("drivers").join("pn532_i2c.h").to_str().unwrap())
+			.header(libnfc_dir.join("drivers").join("pn532_spi.h").to_str().unwrap())
+			.header(libnfc_dir.join("drivers").join("pn532_uart.h").to_str().unwrap())
+			.header(libnfc_dir.join("drivers").join("pn71xx.h").to_str().unwrap())
+			.allowlist_function("pn(53[x23]|71xx)_.*")
+			.allowlist_type("pn(53[x23]|71xx)_.*")
+			.allowlist_var("PN(53[Xx23]|71XX)_.*")
+			.header(libnfc_dir.join("drivers").join("pcsc.h").to_str().unwrap())
+			.allowlist_function("pcsc_.*")
+			.allowlist_type("pcsc_.*")
+			.allowlist_var("PCSC_.*");
+	}
 
 	bindings
+		.generate()
+		.expect("Unable to generate nfc bindings")
 		.write_to_file(out_dir.join("bindings.rs"))
 		.expect("Unable to write nfc bindings");
 }
